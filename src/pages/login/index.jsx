fb5540a1edb7c32147fac95d91969507cfedbb53
tbathat <tabathapradier@gmail.com>
2023-07-09 16:20:27 -0300

    feat: start project

---
+++ src/pages/login/index.jsx
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom";
+
+const Login = () => {
+  return (
+    <>
+      <h1>Login</h1>
+      <Link to={"/"}>Voltar para Home</Link>
+    </>
+  );
+};
+
+export { Login };
