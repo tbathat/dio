3c62e4b2cd4696a54951b0429bafc7647990837c
tbathat <tabathapradier@gmail.com>
2023-07-10 20:28:35 -0300

    fix: button font-family

--- src/components/Button/styles.js
+++ src/components/Button/styles.js
@@ -13,20 +13,25 @@ export const ButtonContainer = styled.button`
   ${({ variant }) =>
     variant !== "primary" &&
     css`
-    min-width: 167px;
-    height: 33px;
+      min-width: 167px;
+      height: 33px;
 
-    background: #E4105D;
+      background: #e4105d;
 
-    &::after {
-      content: '',
-      position: absolute,
-      border: 1px solid #E4105D;
-      top: -5px;
-      left: -6px;
-      width: calc(100% - 10px);
-      height: calc(100% + 10px);
-      border-radius: 22px;
-    }
-  `}
+      &:hover {
+        opacity: 0.6;
+        cursor: pointer;
+      }
+
+      &::after {
+        content: "";
+        position: absolute;
+        border: 1px solid #e4105d;
+        top: -5px;
+        left: -6px;
+        width: calc(100% + 10px);
+        height: calc(100% + 10px);
+        border-radius: 22px;
+      }
+    `}
 `;
