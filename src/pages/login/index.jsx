b2d7fdcd4153ad2bd79da4c2277a7457c57bb77f
tbathat <tabathapradier@gmail.com>
2023-07-10 21:18:17 -0300

    feat: login page

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -1,12 +1,47 @@
-import { Link } from "react-router-dom";
+import { Header } from "../../components/Header";
+import { Input } from "../../components/Input";
+import { Button } from "../../components/Button";
+import {
+  Column,
+  Container,
+  CreateAccountText,
+  ForgotPasswordText,
+  Row,
+  SubtitleLogin,
+  Title,
+  TitleLogin,
+  Wrapper,
+} from "./styles";
 
 const Login = () => {
   return (
     <>
-      <h1>Login</h1>
-      <Link to={"/"}>Voltar para Home</Link>
+      <Header />
+      <Container>
+        <Column>
+          <Title>
+            A plataforma para você aprender com experts, dominar as principais
+            tecnologias do mercado e entrar mais rápido nas empresas mais
+            desejadas.
+          </Title>
+        </Column>
+        <Column>
+          <Wrapper>
+            <TitleLogin>Faça seu cadastro</TitleLogin>
+            <SubtitleLogin>Faça seu login e make the change._</SubtitleLogin>
+            <form>
+              <Input placeholder="E-mail" />
+              <Input placeholder="Senha" type="password" />
+              <Button title="Entrar" variant="secondary" />
+            </form>
+            <Row>
+              <ForgotPasswordText>Esqueci minha senha</ForgotPasswordText>
+              <CreateAccountText>Criar Conta</CreateAccountText>
+            </Row>
+          </Wrapper>
+        </Column>
+      </Container>
     </>
   );
 };
-
 export { Login };
